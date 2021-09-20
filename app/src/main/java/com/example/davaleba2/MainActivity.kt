0569b503aeac7a0dee10fb97e9f961d5ecacba00
d-ava <d.avalishvili@gmail.com>
2021-09-20 19:31:15 +0400

    Initial commit

---
+++ app/src/main/java/com/example/davaleba2/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.davaleba2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val eText = findViewById<EditText>(R.id.eText)
+        val tView = findViewById<TextView>(R.id.tView)
+        var str = ""
+
+        button.setOnClickListener(View.OnClickListener {
+            str = eText.text.toString().reversed()
+            tView.text = str
+        })
+
+    }
+}
